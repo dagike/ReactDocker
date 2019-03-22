5660b59115a581cc6455f1a53d3727ff7cf97a45
DAGIKE <isaac20053@hotmail.com>
2019-03-22 19:47:16 -0400

    minor fixes to api

--- week10/api/index.js
+++ week10/api/index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 
-const DBName = process.env.NODE_ENV === "prod" ? "my-mongo" : "10.0.75.1";
+//const DBName = process.env.NODE_ENV === "prod" ? "my-mongo" : "10.0.75.1";
 
+const DBName = "games";
 const url = `mongodb://10.0.75.1/${DBName}`;
 
 //const url = `mongodb://admin:secret@${dbName}:27017`;
@@ -14,7 +15,7 @@ const options = {
   reconnectInterval: 1000
 };
 const routes = require("./routes/routes.js");
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 const app = express();
 const http = require("http").Server(app);
 
