b77b86847992f276f2b724abd8e039f92dd90365
DAGIKE <isaac20053@hotmail.com>
2019-03-29 12:21:14 -0400

    add database dockercompose

---
+++ week10/database/mongo-init.js
@@ -0,0 +1,32 @@
+const gameData = [
+  {
+    title: "God of War",
+    genre: "Action",
+    year: 2018,
+    consoles: ["PS4"],
+    developer: "SIE Santa Monica Studio"
+  },
+  {
+    title: "Monster Hunter World",
+    genre: "Action",
+    year: 2018,
+    consoles: ["PS4", "XB1", "PC"],
+    developer: "Capcom"
+  },
+  {
+    title: "Detriot: Become Human",
+    genre: "Adventure",
+    year: 2018,
+    consoles: ["PS4"],
+    developer: "Quantic Dream"
+  },
+  {
+    title: "Sunset Overdrive",
+    genre: "Adventure",
+    year: 2018,
+    consoles: ["XONE", "PC"],
+    developer: "Insomniac Games"
+  }
+];
+
+db.instructors.insertMany(gameData);
